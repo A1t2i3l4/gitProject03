e92a910fdf62e785e1865d01bf428e675ab6378d
Atilla <karagoz.atilla@gmail.com>
2023-01-26 10:34:41 -0600

    ilk gorevimi yaptim ikinci gorevi yaptim

--- src/Gun01/Ogrenci.java
+++ src/Gun01/Ogrenci.java
@@ -1,7 +1,10 @@
 package Gun01;
 
+import java.util.Scanner;
+
 public class Ogrenci {
     public static void main(String[] args) {
-
+        Scanner scanner= new Scanner(System.in);
+        System.out.println("Ekrana yazi yazdiriyorum");
     }
 }
