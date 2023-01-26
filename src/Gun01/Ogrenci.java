e1a82091cd627a0d58f8de103485b17c60139be0
Atilla <karagoz.atilla@gmail.com>
2023-01-26 10:40:52 -0600

    ucuncu gorev buydu

--- src/Gun01/Ogrenci.java
+++ src/Gun01/Ogrenci.java
@@ -6,5 +6,7 @@ public class Ogrenci {
     public static void main(String[] args) {
         Scanner scanner= new Scanner(System.in);
         System.out.println("Ekrana yazi yazdiriyorum");
+
+        System.out.println("Yeni bir sey ekledim...3.commit");
     }
 }
